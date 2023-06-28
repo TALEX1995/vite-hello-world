e83f1eb5b8c05b1196adcb9d03de3bb2a6e670de
TALEX1995 <tanase.alex@libero.it>
2023-06-28 15:29:42 +0200

    Added AppTitle

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    data() {
+        return {
+            text: 'La mia prima app con Vite!'
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>{{ text }}</h1>
+</template>
