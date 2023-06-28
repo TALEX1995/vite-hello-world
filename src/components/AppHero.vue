4b1391436ecddc6fde6f526dd5fb814b79d5e758
TALEX1995 <tanase.alex@libero.it>
2023-06-28 15:53:35 +0200

    Added jumbo

---
+++ src/components/AppHero.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+    data() {
+        return {
+            jumboUrl: 'meme'
+        }
+    },
+
+    methods: {
+        getMemeUrl(pickName) {
+            const url = new URL(`../assets/${pickName}.png`, import.meta.url)
+
+            return url.href
+        }
+    }
+}
+</script>
+
+
+
+<template>
+    <div class="container">
+        <div class="jumbo">
+            <img :src="getMemeUrl(jumboUrl)" alt="">
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.container {
+    width: 100%;
+    background-color: blue;
+}
+
+.jumbo {
+    width: 60%;
+    padding: 50px;
+    margin: auto;
+}
+
+img {
+    width: 100%;
+}
+</style>
